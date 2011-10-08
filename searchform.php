f81613d880d9555a5b290d8d09e8a1436d80c6ee
Johnny Taylor <johnny@abledaccess.com>
2011-10-08 18:44:36 -0400

    Minor changes to searchform and style

--- searchform.php
+++ searchform.php
@@ -2,6 +2,6 @@
 						<form id="search-form" method="get" action="<?php bloginfo('url'); ?>/">
 							<label for="s" class="fl-hidden-accessible">Search terms</label>
 							<input id="s" name="s" value="<?php the_search_query(); ?>" type="text" />
-							<input type="submit" class="btn" value="Find it!" />
+							<input type="submit" class="btn" value="Search!" />
 						</form>
 					</nav>
